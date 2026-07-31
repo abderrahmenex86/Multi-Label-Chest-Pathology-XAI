4281be1eb45a28414a17e5f628afccb71c4795fa
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-07-31 08:06:14 +0100

    [dataset.py] replacement of PIL with openCV for fast C++ image decoding

--- src/dataset.py
+++ src/dataset.py
@@ -1,10 +1,11 @@
 import os
 
-import pandas
+import cv2
 import torch
-from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
+
+cv2.setNumThreads(0)
 
 
 class ChestDataset(Dataset):
@@ -31,6 +32,7 @@ class ChestDataset(Dataset):
         if augment:
             self.transform = transforms.Compose(
                 [
+                    transforms.ToPILImage(),
                     transforms.Resize((height, width)),
                     transforms.RandomHorizontalFlip(p=0.5),
                     transforms.RandomRotation(degrees=10),
@@ -41,6 +43,7 @@ class ChestDataset(Dataset):
         else:
             self.transform = transforms.Compose(
                 [
+                    transforms.ToPILImage(),
                     transforms.Resize((height, width)),
                     transforms.ToTensor(),
                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -54,8 +57,11 @@ class ChestDataset(Dataset):
         record = self.records[index]
         filename = record["Image Index"] if "Image Index" in record else record["Image_Index"]
         path = os.path.join(self.directory, filename)
-        image = Image.open(path).convert("RGB")
-        inputs = self.transform(image)
+
+        bgr = cv2.imread(path, cv2.IMREAD_COLOR)
+        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+
+        inputs = self.transform(rgb)
         targets = torch.tensor([float(record[col]) for col in self.columns], dtype=torch.float32)
 
         return inputs, targets
