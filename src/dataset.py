ada29a21c5bacdb96853d2560d7144127bd22e14
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-07-31 08:17:43 +0100

    [dataset.py] replace PIL transforms with OpenCV C++ routines to eliminate garbage collection pauses

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,8 @@
 import os
 
 import cv2
+import numpy
+import pandas
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -11,6 +13,9 @@ cv2.setNumThreads(0)
 class ChestDataset(Dataset):
     def __init__(self, metadata, directory, width, height, augment):
         self.directory = directory
+        self.width = width
+        self.height = height
+        self.augment = augment
         self.records = metadata.to_dict("records")
         self.columns = [
             "Atelectasis",
@@ -28,27 +33,7 @@ class ChestDataset(Dataset):
             "Pleural_Thickening",
             "Hernia",
         ]
-
-        if augment:
-            self.transform = transforms.Compose(
-                [
-                    transforms.ToPILImage(),
-                    transforms.Resize((height, width)),
-                    transforms.RandomHorizontalFlip(p=0.5),
-                    transforms.RandomRotation(degrees=10),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ]
-            )
-        else:
-            self.transform = transforms.Compose(
-                [
-                    transforms.ToPILImage(),
-                    transforms.Resize((height, width)),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ]
-            )
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def __len__(self):
         return len(self.records)
@@ -60,8 +45,19 @@ class ChestDataset(Dataset):
 
         bgr = cv2.imread(path, cv2.IMREAD_COLOR)
         rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+        resized = cv2.resize(rgb, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
 
-        inputs = self.transform(rgb)
+        if self.augment:
+            if numpy.random.rand() > 0.5:
+                resized = cv2.flip(resized, 1)
+
+            angle = numpy.random.uniform(-10, 10)
+            center = (self.width // 2, self.height // 2)
+            matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+            resized = cv2.warpAffine(resized, matrix, (self.width, self.height))
+
+        tensor = torch.from_numpy(resized).permute(2, 0, 1).float() / 255.0
+        inputs = self.normalize(tensor)
         targets = torch.tensor([float(record[col]) for col in self.columns], dtype=torch.float32)
 
         return inputs, targets
