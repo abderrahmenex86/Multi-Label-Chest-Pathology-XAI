a3cddef80c427ddb7909f1773327d75f049014bf
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-07-31 03:31:24 +0100

    [dataset.py] standard pytorch dataset

---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+import os
+
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class ChestDataset(Dataset):
+    def __init__(self, metadata, directory, width, height, augment):
+        self.directory = directory
+        self.records = metadata.to_dict("records")
+        self.columns = [
+            "Atelectasis",
+            "Cardiomegaly",
+            "Effusion",
+            "Infiltration",
+            "Mass",
+            "Nodule",
+            "Pneumonia",
+            "Pneumothorax",
+            "Consolidation",
+            "Edema",
+            "Emphysema",
+            "Fibrosis",
+            "Pleural_Thickening",
+            "Hernia",
+        ]
+
+        if augment:
+            self.transform = transforms.Compose(
+                [
+                    transforms.Resize((height, width)),
+                    transforms.RandomHorizontalFlip(p=0.5),
+                    transforms.RandomRotation(degrees=10),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+        else:
+            self.transform = transforms.Compose(
+                [
+                    transforms.Resize((height, width)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+
+    def __len__(self):
+        return len(self.records)
+
+    def __getitem__(self, index):
+        record = self.records[index]
+        path = os.path.join(self.directory, record["Image_Index"])
+        image = Image.open(path).convert("RGB")
+        inputs = self.transform(image)
+        targets = torch.tensor([float(record[col]) for col in self.columns], dtype=torch.float32)
+
+        return inputs, targets
