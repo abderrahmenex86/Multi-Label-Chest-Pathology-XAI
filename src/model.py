4ffa56eb32c7a9be22dc6da4ac1f4a9eba7d3f5c
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-07-31 03:39:51 +0100

    [model.py] add densenet161 architecture option natively

--- src/model.py
+++ src/model.py
@@ -1,11 +1,21 @@
 import torch
-from torchvision.models import densenet121, DenseNet121_Weights
+from torchvision.models import (
+    DenseNet121_Weights,
+    DenseNet161_Weights,
+    densenet121,
+    densenet161,
+)
 
 
 class DenseNetMultiLabel(torch.nn.Module):
-    def __init__(self, classes):
+    def __init__(self, classes, architecture="densenet121"):
         super().__init__()
-        self.backbone = densenet121(weights=DenseNet121_Weights.DEFAULT)
+
+        if architecture == "densenet161":
+            self.backbone = densenet161(weights=DenseNet161_Weights.DEFAULT)
+        else:
+            self.backbone = densenet121(weights=DenseNet121_Weights.DEFAULT)
+
         in_features = self.backbone.classifier.in_features
         self.backbone.classifier = torch.nn.Linear(in_features, classes)
 
