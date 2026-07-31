90e410413c3d47d06aca91780ad3eea431e8f9c3
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-07-31 14:22:01 +0100

    [dataset.py] refactor dataset to use torchvision transforms v2 directly on cached uint8 tensors

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,10 @@
 import os
 
 import cv2
-import numpy
-import pandas
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
+from torchvision.transforms import v2
+from tqdm import tqdm
 
 cv2.setNumThreads(0)
 
@@ -33,7 +32,24 @@ class ChestDataset(Dataset):
             "Pleural_Thickening",
             "Hernia",
         ]
-        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        if augment:
+            self.transform = v2.Compose(
+                [
+                    v2.RandomHorizontalFlip(p=0.5),
+                    v2.RandomRotation(degrees=15),
+                    v2.ColorJitter(brightness=0.2, contrast=0.2),
+                    v2.ToDtype(torch.float32, scale=True),
+                    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
+        else:
+            self.transform = v2.Compose(
+                [
+                    v2.ToDtype(torch.float32, scale=True),
+                    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ]
+            )
 
         self.cache = cache
         if self.cache:
@@ -55,29 +71,15 @@ class ChestDataset(Dataset):
 
         if self.cache:
             image_tensor = self.images[index]
-            rgb = image_tensor.permute(1, 2, 0).numpy()
         else:
             filename = record["Image Index"] if "Image Index" in record else record["Image_Index"]
             path = os.path.join(self.directory, filename)
             bgr = cv2.imread(path, cv2.IMREAD_COLOR)
             rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-            rgb = cv2.resize(rgb, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
+            resized = cv2.resize(rgb, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
+            image_tensor = torch.from_numpy(resized).permute(2, 0, 1)
 
-        if self.augment:
-            if numpy.random.rand() > 0.5:
-                rgb = cv2.flip(rgb, 1)
-
-            angle = numpy.random.uniform(-15, 15)
-            center = (self.width // 2, self.height // 2)
-            matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-            rgb = cv2.warpAffine(rgb, matrix, (self.width, self.height))
-
-            alpha = numpy.random.uniform(0.8, 1.2)
-            beta = numpy.random.uniform(-15, 15)
-            rgb = cv2.convertScaleAbs(rgb, alpha=alpha, beta=beta)
-
-        tensor = torch.from_numpy(rgb).permute(2, 0, 1).float() / 255.0
-        inputs = self.normalize(tensor)
+        inputs = self.transform(image_tensor)
         targets = torch.tensor([float(record[col]) for col in self.columns], dtype=torch.float32)
 
         return inputs, targets
