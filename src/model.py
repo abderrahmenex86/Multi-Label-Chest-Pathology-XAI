b287ea108e6455ec36f238d249fb5b21f09ba676
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-08-01 04:04:40 +0100

    [model.py] add densenet161 resnet50 efficientnet b2 and convnext small larger architecture support

--- src/model.py
+++ src/model.py
@@ -1,15 +1,21 @@
 import torch
 from torchvision.models import (
+    ConvNeXt_Small_Weights,
     DenseNet121_Weights,
     DenseNet161_Weights,
     EfficientNet_B0_Weights,
+    EfficientNet_B2_Weights,
     MobileNet_V3_Small_Weights,
     ResNet18_Weights,
+    ResNet50_Weights,
+    convnext_small,
     densenet121,
     densenet161,
     efficientnet_b0,
+    efficientnet_b2,
     mobilenet_v3_small,
     resnet18,
+    resnet50,
 )
 
 
@@ -22,6 +28,18 @@ class DenseNetMultiLabel(torch.nn.Module):
             self.backbone = densenet161(weights=DenseNet161_Weights.DEFAULT)
             in_features = self.backbone.classifier.in_features
             self.backbone.classifier = torch.nn.Linear(in_features, classes)
+        elif architecture == "resnet50":
+            self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+            in_features = self.backbone.fc.in_features
+            self.backbone.fc = torch.nn.Linear(in_features, classes)
+        elif architecture == "efficientnet_b2":
+            self.backbone = efficientnet_b2(weights=EfficientNet_B2_Weights.DEFAULT)
+            in_features = self.backbone.classifier[1].in_features
+            self.backbone.classifier[1] = torch.nn.Linear(in_features, classes)
+        elif architecture == "convnext_small":
+            self.backbone = convnext_small(weights=ConvNeXt_Small_Weights.DEFAULT)
+            in_features = self.backbone.classifier[2].in_features
+            self.backbone.classifier[2] = torch.nn.Linear(in_features, classes)
         elif architecture == "resnet18":
             self.backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
             in_features = self.backbone.fc.in_features
@@ -47,6 +65,8 @@ class DenseNetMultiLabel(torch.nn.Module):
     def target_layer(self):
         if "resnet" in self.architecture:
             return self.backbone.layer4
+        elif "convnext" in self.architecture or "efficientnet" in self.architecture or "densenet" in self.architecture:
+            return self.backbone.features
         return self.backbone.features
 
     def forward(self, inputs):
